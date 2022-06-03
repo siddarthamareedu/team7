f58ca0e3b52912089d860df4167e2ccdf9b218cc
Siddarth <mareedusiddartha@gmail.com>
2022-06-03 23:12:03 +0530

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/users");
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+const DB =
+  "mongodb+srv://team7:team7_123@cluster0.l793gjv.mongodb.net/mern?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log(`connection successful`);
+  })
+  .catch((err) => console.log(`no connection`));
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection successful");
+});
+
+app.get("/getUsers", async (req, res) => {
+  const allUser = await UserModel.find({});
+  // console.log(`${allUser}`);
+  res.send(allUser);
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+app.listen(3001, () => {
+  console.log("Server Running");
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
+function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+    });
+  }, []);
+  return (
+    <div className="App">
+      <div className="usersDisplay">
+        {listOfUsers.map((user) => {
+          return (
+            <div>
+              <h1>Name:{user.name}</h1>
+              <h1>Age:{user.age}</h1>
+              <h1>Username:{user.username}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
